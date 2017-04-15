a970529fd2f04ad403bdbc74abb8d755a676e194
EternalMelody <mario.andika@gmail.com>
2017-04-14 21:25:19 -0700

    Implemented audio rms calculation and plotting

--- src/plotter.py
+++ src/plotter.py
@@ -1,71 +1,73 @@
 from queue import Queue
 from pyqtgraph.Qt import QtGui, QtCore
-import pyqtgraph as pg
+import pyqtgraph
 from recorder import Recorder
 import audioop
 
-class MyPlotter(QtGui.QWidget):
-    def __init__(self):
+
+# Plots sound graph from microphone
+class Plotter(QtGui.QWidget):
+    RMS_QUEUE_SIZE = 100  # RMS queue size in timer intervals
+    TIMER_INTERVAL = 100  # Timer interval in milliseconds
+
+    def __init__(self, width, height):
         QtGui.QWidget.__init__(self)
-        self.initUI()
-        self.initData()
-        self.initPlotter()
 
-    def initUI(self):
-        win = pg.GraphicsWindow(title="Basic plotting examples")
-        win.resize(1000, 600)
-        win.setWindowTitle('pyqtgraph example: Plotting')
-
-        # Enable antialiasing for prettier plots
-        pg.setConfigOptions(antialias=True)
-
-        # p6 = win.addPlot(title="Updating plot")
-        p6 = pg.PlotWidget()
-        curve = p6.plot(pen='y')
-        p6.enableAutoRange('xy', True)
-        self.curve = curve
-
-        ## Create some widgets
+        # Initialize the user interface
+        graphics_window = pyqtgraph.GraphicsWindow()
         layout = QtGui.QGridLayout()
-        label = QtGui.QLabel()
-        label.setText("No input")
-        label.setAlignment(QtCore.Qt.AlignCenter)
-        label.setStyleSheet("QLabel { background-color : white; color : black; }")
-        layout.addWidget(label, 1, 0)
-        layout.addWidget(p6, 0, 0)
-
         self.setLayout(layout)
-        self.resize(1000, 600)
-        self.setWindowTitle('LiveFFT')
+        self.resize(width, height)
+        self.setWindowTitle('PyAudio Demo')
+
+        # Create plots
+        pyqtgraph.setConfigOptions(antialias=True)
+        oscilloscope_widget = pyqtgraph.PlotWidget()
+        oscilloscope_curve = oscilloscope_widget.plot(pen='y')
+        oscilloscope_widget.setXRange(0,1050)
+        oscilloscope_widget.setYRange(-50000,50000)
+        self.oscilloscope_curve = oscilloscope_curve
+
+        volume_widget = pyqtgraph.PlotWidget()
+        volume_curve = volume_widget.plot(pen='y')
+        volume_widget.setYRange(0, 16000)
+        volume_widget.setXRange(0, Plotter.RMS_QUEUE_SIZE)
+        self.volume_curve = volume_curve
+
+        # Create other widgets
+        layout.addWidget(oscilloscope_widget, 0, 0)
+        layout.addWidget(volume_widget,0, 1)
+
         self.show()
         timer = QtCore.QTimer()
-        timer.timeout.connect(self.handleNewData)
-        timer.start(100)
-        # keep reference to timer
+        timer.timeout.connect(self.handle_new_data)
+        timer.start(Plotter.TIMER_INTERVAL)
         self.timer = timer
 
-    def initData(self):
-        rec = Recorder()
-        rec.start()
-        self.rec = rec
-        self.rms = 0.0#audioop.rms(rec.frames[0],2)
+        # Initialize the recorder
+        recorder = Recorder()
+        recorder.start()
+        self.recorder = recorder
+        self.rms_queue = Queue(maxsize=Plotter.RMS_QUEUE_SIZE)
 
-    def initPlotter(self):
-        pass
-
-    def handleNewData(self):
-        frames = self.rec.get_frames()
+    # Handle callback data from recorder
+    def handle_new_data(self):
+        frames = self.recorder.get_frames()
         if len(frames) > 0:
             current_frame = frames[-1]
-            self.curve.setData(current_frame)
+            rms = audioop.rms(current_frame,2)
+            if self.rms_queue.full():
+                self.rms_queue.get_nowait()
+            self.rms_queue.put_nowait(rms)
+            self.oscilloscope_curve.setData(current_frame)
+            self.volume_curve.setData(list(self.rms_queue.queue))
 
 
-# TODO Move to main.py
 # Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        # QtGui.QApplication.instance().exec_()
         app = QtGui.QApplication(sys.argv)
-        window = MyPlotter()
+        screen_rect = app.desktop().screenGeometry()
+        window = Plotter(screen_rect.width(), screen_rect.height())
         sys.exit(app.exec_())
--- src/recorder.py
+++ src/recorder.py
@@ -1,34 +1,39 @@
 import threading
-import wave
 import pyaudio
-import audioop
 import atexit
-import numpy as np
+import numpy
 
+
+# On creation, records audio through the microphone
 class Recorder(object):
     def __init__(self, rate=4000, chunksize=1024):
+        # Initialize audio variables and pyaudio
         self.rate = rate
         self.chunksize = chunksize
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(format=pyaudio.paInt16,
-                                  channels=1,
-                                  rate=self.rate,
-                                  input=True,
-                                  frames_per_buffer=self.chunksize,
-                                  stream_callback=self.new_frame)
+        self.pyaudio_instance = pyaudio.PyAudio()
+        self.stream = self.pyaudio_instance.open(format=pyaudio.paInt16,
+                                                 channels=1,
+                                                 rate=self.rate,
+                                                 input=True,
+                                                 frames_per_buffer=self.chunksize,
+                                                 stream_callback=self.new_frame)
+        self.frames = []
+
+        # Initialize multithreading routines
         self.lock = threading.Lock()
         self.stop = False
-        self.frames = []
         atexit.register(self.close)
 
+    # Callback method when a new audio frame is recorded
     def new_frame(self, data, frame_count, time_info, status):
-        data = np.fromstring(data, 'int16')
+        data = numpy.fromstring(data, 'int16')
         with self.lock:
             self.frames.append(data)
             if self.stop:
                 return None, pyaudio.paComplete
         return None, pyaudio.paContinue
 
+    # Return filled audio frames, and re-initialize audio frames
     def get_frames(self):
         with self.lock:
             frames = self.frames
@@ -38,49 +43,9 @@ class Recorder(object):
     def start(self):
         self.stream.start_stream()
 
+    # Stop recording, close the stream, terminate pyaudio
     def close(self):
         with self.lock:
             self.stop = True
         self.stream.close()
-        self.p.terminate()
-#     CHUNK = 1024
-#     FORMAT = pyaudio.paInt16
-#     CHANNELS = 2
-#     RATE = 44100
-#     WAVE_OUTPUT_FILENAME = "apt.wav"
-#
-#     def __init__(self):
-#         super(Recorder, self).__init__()
-#         self.p = pyaudio.PyAudio()
-#
-#         self.stream = self.p.open(format=self.FORMAT,
-#                         channels=self.CHANNELS,
-#                         rate=self.RATE,
-#                         input=True,
-#                         frames_per_buffer=self.CHUNK)
-#
-#         self.wf = wave.open(self.WAVE_OUTPUT_FILENAME, 'wb')
-#         self.wf.setnchannels(self.CHANNELS)
-#         self.wf.setsampwidth(self.p.get_sample_size(self.FORMAT))
-#         self.wf.setframerate(self.RATE)
-#         self.rms = 0.0
-#
-#     def run(self):
-#         print("* recording")
-#         try:
-#             while True:
-#                 frames = []
-#                 data = self.stream.read(self.CHUNK)
-#                 self.rms = audioop.rms(data,2)
-#                 frames.append(data)
-#                 self.wf.writeframes(b''.join(frames))
-#         finally:
-#             # clean up
-#             print("* done recording")
-#             self.stream.close()
-#             self.p.terminate()
-#             self.wf.close()
-#
-#
-# if __name__ == '__main__':
-#     Recorder().run()
+        self.pyaudio_instance.terminate()
